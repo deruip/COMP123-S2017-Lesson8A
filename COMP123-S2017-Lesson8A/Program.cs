e5b8774c03f61c378b4d4dd2d10b3da55c93fa71
deruip <aronly1997@hotmail.com>
2017-06-29 11:25:19 -0400

    intial commit

---
+++ COMP123-S2017-Lesson8A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2017_Lesson8A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("test");
+        }
+    }
+}
